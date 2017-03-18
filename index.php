36f777123f29492edeb0bf730399f6ffe8848e20
KingDragon08 <13810332931@163.com>
2017-03-18 14:56:27 +0800

    cnu init

---
+++ index.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, height=device-height">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css" media="screen">
+    <title>网站名字</title>
+  </head>
+  <body>
+    <div class="full_page">
+      <div class="video">
+        <video src="img/eg.mp4" autoplay="autoplay" loop="loop" preload="load"></video>
+      </div>
+      <div class="info">
+        <div class="container">
+          <p class="big_font">
+            我是一些简单的文字信息我是一些简单的文字信息
+            我是一些简单的文字信息我是一些简单的文字信息
+          </p>
+          <a href="#" class="learn_more_btn">了解更多</a>
+        </div>
+      </div>
+      <div class="nav">
+        <div class="logo">LOGO</div>
+        <ul>
+          <li>
+            <a href="index.php" class="active">首页</a>
+          </li>
+          <li>
+            <a href="yingbi.php">硬笔</a>
+          </li>
+          <li>
+            <a href="ruanbi.php">软笔</a>
+          </li>
+          <li>
+            <a href="#">名家</a>
+          </li>
+          <li>
+            <a href="#">论坛</a>
+          </li>
+          <li>
+            <a href="#">个人中心</a>
+          </li>
+          <li>
+            <a href="#">联系我们</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="container">
+      <div class="head">名家</div>
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt20">
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb10">
+          <div class="col-lg-6">
+            <img src="img/professors/1.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>姓名:<b>我是姓名</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">了解更多</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb10">
+          <div class="col-lg-6">
+            <img src="img/professors/1.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>姓名:<b>我是姓名</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">了解更多</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb10">
+          <div class="col-lg-6">
+            <img src="img/professors/1.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>姓名:<b>我是姓名</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">了解更多</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container mt10">
+      <div class="head">软笔</div>
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt20">
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/1.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>颜真卿多宝塔</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/1.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>颜真卿多宝塔</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/1.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>颜真卿多宝塔</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/1.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>颜真卿多宝塔</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/1.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>颜真卿多宝塔</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/1.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>颜真卿多宝塔</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+    <div class="container mt10">
+      <div class="head">硬笔</div>
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt20">
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/2.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>宋词三百首</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/2.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>宋词三百首</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/2.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>宋词三百首</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/2.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>宋词三百首</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/2.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>宋词三百首</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mb20">
+          <div class="col-lg-6">
+            <img src="img/book/2.jpg" width="100%"/>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <p>书名:<b>宋词三百首</b></p>
+            <p>简介:
+              <small>
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息我是简介信息我是简介信息
+              我是简介信息我是简介信息
+              </small>
+            </p>
+            <a href="#" class="btn btn-danger">配套视频</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="container">
+        <nav class="mt20 mb10">友情连接</nav>
+        <ul>
+          <li><a href="#">友情链接1</a></li>
+          <li><a href="#">友情链接2</a></li>
+          <li><a href="#">友情链接3</a></li>
+          <li><a href="#">友情链接4</a></li>
+          <li><a href="#">友情链接5</a></li>
+          <li><a href="#">友情链接6</a></li>
+          <li><a href="#">友情链接7</a></li>
+          <li><a href="#">友情链接8</a></li>
+          <li><a href="#">友情链接9</a></li>
+          <li><a href="#">友情链接1</a></li>
+          <li><a href="#">友情链接2</a></li>
+          <li><a href="#">友情链接3</a></li>
+          <li><a href="#">友情链接4</a></li>
+          <li><a href="#">友情链接5</a></li>
+          <li><a href="#">友情链接6</a></li>
+          <li><a href="#">友情链接7</a></li>
+          <li><a href="#">友情链接8</a></li>
+        </ul>
+      </div>
+      <div class="container">
+        <nav class="mt20 mb10">合作伙伴</nav>
+        <ul>
+          <li><a href="#">合作伙伴1</a></li>
+          <li><a href="#">合作伙伴2</a></li>
+          <li><a href="#">合作伙伴3</a></li>
+          <li><a href="#">合作伙伴4</a></li>
+          <li><a href="#">合作伙伴5</a></li>
+          <li><a href="#">合作伙伴6</a></li>
+          <li><a href="#">合作伙伴7</a></li>
+          <li><a href="#">合作伙伴8</a></li>
+          <li><a href="#">合作伙伴9</a></li>
+          <li><a href="#">合作伙伴1</a></li>
+          <li><a href="#">合作伙伴2</a></li>
+          <li><a href="#">合作伙伴3</a></li>
+          <li><a href="#">合作伙伴4</a></li>
+          <li><a href="#">合作伙伴5</a></li>
+        </ul>
+      </div>
+      <hr>
+      <p class="icp">CopyRight&copy;我是版权所属单位的名字 2016～2020</p>
+      <p class="icp">京ICP备14045086号-1</p>
+    </div>
+  </body>
+</html>
